5237101414cbf3f6c0d12cbfec24ef2949b4bdbd
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-05 18:10:11 -0400

    Adds test and method sunk?

--- battleship/lib/ship.rb
+++ battleship/lib/ship.rb
@@ -3,11 +3,25 @@ class Ship
                 :length
 
     attr_accessor :health,
-                  :hit
+                  :hit,
+                  :health
 
     def initialize(name, length)
         @name = name
         @length = length
-
+        @health = length
+        @sunk = false
     end
+
+    def sunk?
+        @sunk
+    end
+
+    # def hit
+
+    # end
+
+    # def health
+
+    # end
 end
--- battleship/spec/ship_spec.rb
+++ battleship/spec/ship_spec.rb
@@ -1,11 +1,19 @@
 require './lib/ship'
 
 RSpec.describe Ship do
-    it 'exists' do
-        ship = Ship.new("Cruiser", 3)
+    before(:each) do
+        @cruiser = Ship.new("Cruiser", 3)
+        @submarine = Ship.new("Submarine", 2)
+    end
 
-        expect(ship).to be_an_instance_of(Ship)
-        expect(ship.name).to eq("Cruiser")
-        expect(ship.length).to eq(3)
+    it 'exists' do
+        expect(@cruiser).to be_an_instance_of(Ship)
+        expect(@cruiser.name).to eq("Cruiser")
+        expect(@cruiser.length).to eq(3)
+        expect(@cruiser.health).to eq(3)
+    end
+
+    it "Sunk? should be false to start the game" do
+        expect(@cruiser.sunk?).to be(false)
     end
 end

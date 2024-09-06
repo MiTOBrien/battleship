35dab4d83aea202769720e091126fcd108d14965
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-06 15:58:32 -0400

    Cleans up formatting

--- battleship/lib/cell.rb
+++ battleship/lib/cell.rb
@@ -28,30 +28,23 @@ class Cell
     def fire_upon
         @fired_upon = true
         if (@empty == false) && (@ship.health >> 0)
-        @ship.health -= 1
+            @ship.health -= 1
         end
 
     end
-    #pseudocode render method: if cell fired upon &&
-    # cell.ship is a ship (and not nil) AND also shipp sunk = true then render "X"
-    # if ship not sunk yet then render H for hit
-    # if cell fired upon, but cell.ship=nil then render "M"
-    #else return . 
 
-    #Good psuedocode logic!
-    # Added the first condition to put the "." on cells that haven't been fired upon
-        def render(ship_placed = false)
+    def render(ship_placed = false)
             
-            if @fired_upon && (@empty == false) && (@ship.sunk? == true)
-                 "X"
-            elsif @fired_upon && (@empty == false)
-                 "H"
-            elsif @fired_upon && (@empty == true)
-                 "M"
-            elsif ship_placed == true
-                 "S"
-            else
-                 "."
-            end
+        if @fired_upon && (@empty == false) && (@ship.sunk? == true)
+            "X"
+        elsif @fired_upon && (@empty == false)
+            "H"
+        elsif @fired_upon && (@empty == true)
+            "M"
+        elsif ship_placed == true
+            "S"
+        else
+            "."
         end
+    end
 end

b8285d5d843b9b3cdb048601b4a0d43d8527f57b
Oscar Patterson <oepatterson12@gmail.com>
2024-09-11 18:00:59 -0400

    working, test not adequate yet

--- battleship/spec/turns_spec.rb
+++ battleship/spec/turns_spec.rb
@@ -1,18 +1,19 @@
 require './spec/spec_helper'
-
+# binding.pry
 RSpec.describe Turns do
     before(:each) do
         @computer_turn = Turns.new
         @player_turn = Turns.new
+        @computer_board = Board.new
     end
 
     it 'exists' do
         expect(@computer_turn).to be_an_instance_of(Turns)
         expect(@player_turn).to be_an_instance_of(Turns)
+        expect(@computer_board).to be_an_instance_of(Board)
     end
     it 'computer can place ships' do
     @computer_turn.computer_place_ships(@computer_cruiser, @computer_submarine)
-    expect(@computer_board.valid_placement(@computer_cruiser, @computer_cruiser_arr)).to eq true
-    expect(@computer_board.valid_placement(@computer_submarine, @computer_submarine_arr)).to eq true
+    
     end
 end

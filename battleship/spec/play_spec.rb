fcb37d365c01b8f902c751b28eb813ec66c32dc7
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-11 17:48:31 -0400

    Updated test file

--- battleship/spec/play_spec.rb
+++ battleship/spec/play_spec.rb
@@ -3,6 +3,8 @@ require './spec/spec_helper'
 RSpec.describe Play do
     it 'exists' do
         play_game = Play.new
+        player_board = Board.new
         expect(play_game).to be_an_instance_of(Play)
+        expect(player_board).to be_an_instance_of(Board)
     end
 end
